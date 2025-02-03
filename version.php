5b5d383b21c28d85cd80fcd72e160946ea650dbd
John Lane <46030891+JohnOLane@users.noreply.github.com>
2025-02-03 11:06:17 -0600

    Removing nonsense decimal.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2025041402;
-$plugin->requires  = 2025041400.00; // Moodle 5.0 and up.
+$plugin->requires  = 2025041400; // Moodle 5.0 and up.
 
 $plugin->component = 'mod_questionnaire';
 
